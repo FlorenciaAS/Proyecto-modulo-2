cd9fcb8463150ac1b95ef17ca16abe8803a8de77
Flor Alcaide <natashaflorencia109@gmail.com>
2020-08-17 17:24:02 -0300

    navbar

--- proyecto modulo-2/alcaide-florencia/src/Components/LoginNavbar.js
+++ proyecto modulo-2/alcaide-florencia/src/Components/LoginNavbar.js
@@ -2,14 +2,15 @@ import React from  'react';
 import {AppBar} from '@material-ui/core';
 import {Toolbar} from '@material-ui/core';
 import { Typography } from '@material-ui/core';
-import iconMatea from '../images/LogoSoloBlancoFondoTransparente 1.png'
+import iconMatea from '../images/LogoSoloBlancoFondoTransparente 1.png';
+import '../Styles/NavbarLogin.css';
 
 
 function LoginNavbar() {
     return(
-        <AppBar position='static'>
+        <AppBar className='container-nav'  position='static'>
             <Toolbar>
-                <img src={iconMatea}></img>
+                <img className='img-nav' src={iconMatea}></img>
                 <Typography variant='h5' align='center'>
                     Mateify
                 </Typography>
