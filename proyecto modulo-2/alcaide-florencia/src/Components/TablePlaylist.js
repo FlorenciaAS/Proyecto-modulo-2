0e88c1b303bd332f05806a9288719c107dd87c0d
Flor Alcaide <natashaflorencia109@gmail.com>
2020-08-24 18:58:47 -0300

    busqueda de playlist

--- proyecto modulo-2/alcaide-florencia/src/Components/TablePlaylist.js
+++ proyecto modulo-2/alcaide-florencia/src/Components/TablePlaylist.js
@@ -13,7 +13,7 @@ import {
 import SearchIcon from "@material-ui/icons/Search";
 import "../Styles/TablePlaylis.css";
 
-function TablePlaylist() {
+function TablePlaylist(props) {
   return (
     <div>
       <div className="search-cont">
@@ -22,38 +22,17 @@ function TablePlaylist() {
             <IconButton aria-label="search">
               <SearchIcon />
             </IconButton>
-            <InputBase placeholder="Buscar" />
+            <InputBase
+              name="song"
+              placeholder="Buscar"
+              onChange={props.handleInputChange}
+              value={props.inputValue}
+              onKeyPress={props.handleInputKeyPress}
+            />
           </Paper>
         </Box>
       </div>
-
-      <div className='cont-table'>
-        <Paper className='table-cont' >
-          <Table aria-label="simple table"  variant='outlined'>
-            <TableHead >
-            <h4>Resultados</h4>
-              <TableRow>
-                <TableCell >Nombre</TableCell>
-                <TableCell align="center">Artista</TableCell>
-                <TableCell align="center">Álbum</TableCell>
-                <TableCell align="center">Duración</TableCell>
-                <TableCell align="center">Agregar</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell >fevdev</TableCell>
-                <TableCell align='center'>veveve</TableCell>
-                <TableCell align='center'>sdfesf</TableCell>
-                <TableCell align='center'>feffef</TableCell>
-                <TableCell align='center'>fefefefe</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </Paper>
-      </div>
-
     </div>
   );
-}
+};
 export default TablePlaylist;
--- proyecto modulo-2/alcaide-florencia/src/functioning/SearchPlaylist.js
+++ proyecto modulo-2/alcaide-florencia/src/functioning/SearchPlaylist.js
@@ -1,17 +1,79 @@
-import React, { useState } from 'react';
-import songData from '../baseDeDatos/songData.json';
-import TablePlaylist from '../Components/TablePlaylist'
+import React, { useState } from "react";
+import {
+  Table,
+  TableRow,
+  TableHead,
+  TableCell,
+  TableBody,
+  Paper,
+} from "@material-ui/core";
+import '../Styles/searchPlaylist.css';
+import songData from "../baseDeDatos/songData.json";
+import TablePlaylist from "../Components/TablePlaylist";
 
 const SearchPlaylist = () => {
-    const [inputValue, setInputValue] = useState('');
-    
+  const [inputValue, setInputValue] = useState("");
+  const [tableFilter, setTableFilter] = useState([]);
 
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
+  const filterSong = (song) => {
+    if (song.name.includes(inputValue)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
-    return(
-        <TablePlaylist/>
-    );
+  const handleInputKeyPress = (e) => {
+    if (e.key === "Enter") {
+      const results = songData.filter(filterSong);
 
+      setTableFilter(results);
+    }
+  };
+
+  return (
+    <div>
+      <TablePlaylist
+        handleInputChange={handleInputChange}
+        inputValue={inputValue}
+        handleInputKeyPress={handleInputKeyPress}
+      />
+
+      <div className="cont-table">
+          <Paper className="table-cont">
+            <Table aria-label="simple table" variant="outlined">
+              <TableHead>
+                <h4>Resultados</h4>
+                <TableRow>
+                  <TableCell>Nombre</TableCell>
+                  <TableCell align="center">Artista</TableCell>
+                  <TableCell align="center">Álbum</TableCell>
+                  <TableCell align="center">Duración</TableCell>
+                  <TableCell align="center">Agregar</TableCell>
+                </TableRow>
+              </TableHead>
+          
+              {
+              tableFilter.map((song) => (
+              <TableBody >
+              <TableRow key={song.uuid}>
+                <TableCell>{song.name}</TableCell>
+                <TableCell align="center">{song.artist.name}</TableCell>
+                <TableCell align="center">{song.album}</TableCell>
+                <TableCell align="center">{song.duration}</TableCell>
+                <TableCell align="center"></TableCell>
+              </TableRow>
+            </TableBody>
+              ))
+            }
+            </Table>
+          </Paper>
+        </div>
+      </div>
+  );
 };
-
 export default SearchPlaylist;
